4d4a691da119dc178bcc3a7f6c1c05ad1d0fe73e
enes.kacan <eneskacan8@gmail.com>
2022-02-07 14:43:55 +0300

    Predicates

--- src/main/java/imperative/Main.java
+++ src/main/java/imperative/Main.java
@@ -2,6 +2,7 @@ package imperative;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static imperative.Main.Gender.FEMALE;
 import static imperative.Main.Gender.MALE;
@@ -32,8 +33,10 @@ public class Main {
 
         System.out.println("Declarative approach");
         // Declarative approach
+        Predicate<Person> personPredicate = person -> FEMALE.equals(person.gender);
+
         people.stream()
-                .filter(person -> FEMALE.equals(person.gender))
+                .filter(personPredicate)
                 .forEach(System.out::println);
 
     }
